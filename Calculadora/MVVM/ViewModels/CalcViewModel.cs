ec71a1073672116e75c90f80b7659ef57147f87c
alfferpla <planas.afz@gmail.com>
2024-12-13 18:18:54 +0100

    Intermedio

--- Calculadora/MVVM/ViewModels/CalcViewModel.cs
+++ Calculadora/MVVM/ViewModels/CalcViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,11 +8,29 @@ using System.Windows.Input;
 
 namespace Calculadora.MVVM.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class CalcViewModel
     {
         public string Formula { get; set; }
         public string Result { get; set; } = "0";
         public ICommand OperationCommand =>
             new Command((number) => { Formula += number; });
+        public ICommand ResetCommand => new Command(() =>
+        {
+            Result = "0";
+            Formula = "";
+        });
+
+        public ICommand BackspaceCommand => new Command(() =>
+        {
+            if(Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }
+        });
+        public ICommand CalculateCommand => new Command(() =>
+        {
+
+        });
     }
 }
